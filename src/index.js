f24900b072b9d8c61cff6b0fe6f08041f451dbe5
Anton Kapustin <antonlettuce@gmail.com>
2020-03-11 08:10:42 +0300

    refactor 4

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import parse from './utils/parsers.js';
 import formatter from './formatters/index.js';
 
 const getContent = (filepath) => fs.readFileSync(filepath, 'utf-8');
+const getExtension = (filepath) => path.extname(filepath).slice(1);
 
-const getDiffrence = (a, b) => {
+const makeAst = (a, b) => {
   const uniqKeys = _.union(Object.keys(a), Object.keys(b));
   return uniqKeys.map((name) => {
     const [value1, value2] = [a[name], b[name]];
     if (_.isObject(value1) && _.isObject(value2)) {
-      return { name, status: 'nested', value: getDiffrence(value1, value2) };
+      return { name, status: 'nested', value: makeAst(value1, value2) };
     }
     if (_.has(a, name) && _.has(b, name)) {
       return value1 === value2 ? { name, status: 'default', value: value1 } : {
@@ -24,15 +25,14 @@ const getDiffrence = (a, b) => {
 
 export default (path1, path2, formatType = 'tree') => {
   const format = formatter(formatType);
-  const ending = path.extname(path1);
-  const parseFunc = parse[ending];
+  const dataType = getExtension(path1);
 
   const content1 = getContent(path1);
   const content2 = getContent(path2);
 
-  const parsedData1 = parseFunc(content1);
-  const parsedData2 = parseFunc(content2);
+  const parsedData1 = parse(content1, dataType);
+  const parsedData2 = parse(content2, dataType);
 
-  const tree = getDiffrence(parsedData1, parsedData2);
+  const tree = makeAst(parsedData1, parsedData2);
   return format(tree);
 };
--- src/utils/parsers.js
+++ src/utils/parsers.js
@@ -1,8 +1,10 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-export default {
-  '.json': JSON.parse,
-  '.yml': yaml.safeLoad,
-  '.ini': ini.parse,
+const parserMap = {
+  json: JSON.parse,
+  yml: yaml.safeLoad,
+  ini: ini.parse,
 };
+
+export default (data, format) => parserMap[format](data);
